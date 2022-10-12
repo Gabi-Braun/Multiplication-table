372bdf5b847f56f060a78c33721d76087f02d9cf
Gabi-Braun <115644089+Gabi-Braun@users.noreply.github.com>
2022-10-12 22:24:08 +0800

    Add files via upload

---
+++ Multiplication Table (Reverse).py
@@ -0,0 +1,25 @@
+multiplier = int(input("Multiplier: "))
+temp = multiplier
+static = multiplier
+
+while multiplier > 0 :
+    while temp > 0:
+        print(f"{multiplier} x {temp} = {multiplier*temp}")
+        temp -= 1
+    temp = multiplier
+    print("\n")
+    multiplier -= 1
+        
+
+    
+    
+   
+    
+
+    
+    
+
+
+                                 
+
+
